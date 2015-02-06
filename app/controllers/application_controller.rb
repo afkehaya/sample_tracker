9d2d502f00427b22e822f8917aebeda5fb397656
Alexander Kehaya <afkehaya@gmail.com>
2015-02-06 09:00:56 -0800

    added names to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
+
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+ # Prevent CSRF attacks by raising an exception.
+ # For APIs, you may want to use :null_session instead.
+ protect_from_forgery with: :exception
+ before_filter :configure_permitted_parameters, if: :devise_controller?
+ 
+ include Pundit
 
-  include Pundit
+ after_action :verify_authorized, unless: :devise_controller?
+ before_filter :set_global_search_variable
 
-  after_action :verify_authorized, unless: :devise_controller?
-  before_filter :set_global_search_variable
+	 def set_global_search_variable
+	   @q = Sample.search(params[:q])
+	 end
+	
+protected
 
- def set_global_search_variable
-   @q = Sample.search(params[:q])
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.for(:sign_up) << :name
+   devise_parameter_sanitizer.for(:account_update) << :name
  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202234027) do
+ActiveRecord::Schema.define(version: 20150206165223) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20150202234027) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
